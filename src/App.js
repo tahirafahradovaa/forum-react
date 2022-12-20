ad7e2280bbaae2869c1b6becbd5ba430af39bb03
tahirafahradovaaa <tairafahrad@icloud.com>
2022-12-20 21:11:11 +0400

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
-import Forum from "./companent/Forum";
+// import Forum from "./companent/Forum";
+import TipCalculator from "./companent/TipCalculator";
 
 function App() {
-  return <Forum></Forum>;
+  // return <Forum></Forum>;
+  return <TipCalculator></TipCalculator>;
 }
 
 export default App;
